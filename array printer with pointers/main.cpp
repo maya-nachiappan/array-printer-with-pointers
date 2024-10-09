41e00d0e9890ea3266a754dfa6d02518dbeb48aa
maya-nachiappan <maya.n102304@gmail.com>
2024-10-09 11:05:15 -0700

    prints an array in reverse order using pointers

--- array printer with pointers/main.cpp
+++ array printer with pointers/main.cpp
@@ -4,11 +4,16 @@
 //
 //  Created by maya nachiappan on 10/9/24.
 //
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int numbers[] = {10, 20, 30};
+    int length = sizeof(numbers) / sizeof(int);
+    int* ptr = numbers + length - 1;
+    for (int i = length - 1; i >= 0; --i) {
+        cout << *(ptr - (length - 1 - i)) << " ";
+    }
+    cout << endl;
     return 0;
 }
